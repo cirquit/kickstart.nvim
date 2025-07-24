55ddc2a2376a2db0f3934db57308a6640ee1393e
Alexander Erben <aerben@meta.com>
2025-07-24 15:52:24 -0400

    added qflist func

--- init.lua
+++ init.lua
@@ -492,11 +492,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<C-q>'] = require('telescope.actions').smart_send_to_qflist + require('telescope.actions').open_qflist },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
